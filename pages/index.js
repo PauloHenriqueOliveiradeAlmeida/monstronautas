1ae944c6fe5cd1db31fd387183b75183de7016cf
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2022-03-19 12:00:47 -0300

    [2022-03-19] (12:00) Arrumando Arquivos - Paulo Henrique

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/main.module.css'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <header class={styles.header}>
-        <figure>
-          <img alt="logo"/>
-        </figure>
-      </header>
-
-      <form className={styles.form}>
-        <label>E-mail do Papai ou da Mamãe</label>
-        <input type="email" required/>
-        
-        <label>Quais Animaizinhos?</label>
-        <div>
-          <input type="checkbox"/>Elefante
-          <input type="checkbox"/>Mamaco
-          <input type="checkbox"/>Paraguaio
-        </div>
-      </form>
 
     </div>
   )
