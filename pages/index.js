36194399653d10de9ab997174d2b1a17971d3882
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2022-04-03 20:46:00 -0300

    [2022-04-03] (20:45) Tela de Login 1 modelo - Paulo Henrique Oliveira de Almeida

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import Login from './components/login/login'
+import Image from 'next/image'
+import mascote from '../public/mascote.svg'
 export default function Home() {
   return (
     <div className={styles.container}>
-
+      <div className={styles.login}>
+        <Login/>
+      </div>
     </div>
   )
 }
