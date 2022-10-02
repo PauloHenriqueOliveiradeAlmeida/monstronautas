af5c65fd3f3c18e573904b608750ddfa8d8bb49c
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2022-10-02 12:57:58 -0300

    habilitado ssl na conexão

--- pages/api/connection.js
+++ pages/api/connection.js
@@ -6,7 +6,8 @@ async function Connection(query, values = []) { //query = o que vai fazer, value
         host: process.env.DB_HOST, //endereço ex: localhost, www.exemplo.com.br, etc...
         database: process.env.DB_NAME, //nome do banco ex: responsaveis, alunos, etc...
         user: process.env.DB_USER, //usuário do banco
-        password: process.env.DB_PASSWORD //senha do banco
+        password: process.env.DB_PASSWORD, //senha do banco
+        ssl: true
     });
     const rows = await db.query(query, values);
 
