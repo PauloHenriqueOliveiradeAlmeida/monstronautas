d56b1a8b2b0da9a2a42c149b529b39db45c3bdf3
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2022-10-07 22:12:21 -0300

    correcao de bugs

--- pages/components/cardDev.jsx
+++ pages/components/cardDev.jsx
@@ -4,10 +4,13 @@ import styles from "./cardDev.module.css"
 
  function CardDev(props) {
 
+    const name = props.name.split(" ", 1);
+    const secondName = props.name.substr(props.name.indexOf(" "), props.name.length);
+
     return (
         <div className={styles.card}>
             <Image src={`https://github.com/${props.username}.png`} layout="fixed" width={120} height={120} alt="foto do Desenvolvedor"/>
-            <h2>{props.name.split(" ", 1)}<br/>{props.name.substr(props.name.indexOf(" "), props.name.length)}</h2>
+            <h2>{name}<br/>{secondName}</h2>
             <hr />
             <p>{props.cargo}</p>
 
